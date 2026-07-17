f772be9835ff5d532c0acee7899ff96784923bcc
HERBORISTERIETOUBA <babayarmohamedamine4@gmail.com>
2026-07-17 17:49:43 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,16 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-// Configuration
 const firebaseConfig = {
-  apiKey: "ضع هنا apiKey الحالي كما هو",
+  apiKey: "AIzaSyBDjX9e4vf2utV5SBeUUDT97KojbzmQ0Zo",
   authDomain: "herboristerie-touba.firebaseapp.com",
   projectId: "herboristerie-touba",
   storageBucket: "herboristerie-touba.firebasestorage.app",
   messagingSenderId: "448032949923",
-  appId: "ضع هنا appId الحالي كما هو"
+  appId: "1:448032949923:web:c1d83401e6783edc9b2116"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
